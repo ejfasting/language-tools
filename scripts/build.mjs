b8c43740ae5d2fa7c1ee586bb114d2cfb8c0dbae
ejfasting <ejfasting@gmail.com>
2024-04-16 21:11:40 +0200

    Added superoffice-vscode-core to the repo

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,48 +1,35 @@
 import esbuild from 'esbuild';
 import console from 'node:console';
 import process from 'node:process';
+import path from 'path';
 
 const production = process.argv[2] === '--production';
 const watch = process.argv[2] === '--watch';
-let desktopContext, browserContext, serverContext;
 
-// This is the base config that will be used by both web and desktop versions of the extension
-const baseConfig = {
-    entryPoints: ['./clients/vscode/src/extension.ts'], // the entry point of this extension, 📖 -> https://esbuild.github.io/api/#entry-points
-    bundle: true,
-    external: ['vscode'], // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be packaged, 📖 -> https://esbuild.github.io/api/#external
-    sourcemap: !production,
-    minify: production,
-    target: 'ES2022', // VS Code 1.82 onwards will support ES2022, this also overrides tsconfig.json.
-};
+// Get the index of '--extension' flag in process.argv
+const extensionIndex = process.argv.indexOf('--extension');
+const extension = process.argv[extensionIndex + 1];
 
-// Build both desktop and web versions of the extension in parallel
+const entryPoint = path.resolve("packages", extension, 'src/extension.ts');
+const outfile = path.resolve("packages", extension, 'dist/client.js');
+
+let browserContext;
+
+// Build web versions of the extension
 // By using .context() instead of .build() we can rebuild in watch mode when the source changes
 try {
-    [browserContext, serverContext] = await Promise.all([
-        // https://esbuild.github.io/getting-started/#bundling-for-node
-        // esbuild.context({
-        //     ...baseConfig,
-        //     entryPoints: ['./clients/vscode/src/extension.ts'], // the entry point of this extension, 📖 -> https://esbuild.github.io/api/#entry-points
-        //     outfile: './clients/vscode/dist/extension.js',
-        //     platform: 'node',
-        // }),
-        // If you're building for the browser, you'll need to generate a second bundle which is suitable
-        // https://esbuild.github.io/getting-started/#bundling-for-the-browser
+    [browserContext] = await Promise.all([
         esbuild.context({
-            ...baseConfig,
-            entryPoints: ['./clients/vscode/src/extension.ts'], // the entry point of this extension, 📖 -> https://esbuild.github.io/api/#entry-points
-            outfile: './clients/vscode/dist/client.js',
+            entryPoints: [entryPoint], // the entry point of this extension, 📖 -> https://esbuild.github.io/api/#entry-points
+            bundle: true,
+            external: ['vscode'], // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be packaged, 📖 -> https://esbuild.github.io/api/#external
+            sourcemap: !production,
+            minify: production,
+            target: 'ES2022', // VS Code 1.82 onwards will support ES2022, this also overrides tsconfig.json.
+            outfile: outfile,
             platform: 'browser',
             format: 'cjs',
-        }),
-        esbuild.context({
-            ...baseConfig,
-            entryPoints: ['./server/src/server.ts'], // the entry point of this extension, 📖 -> https://esbuild.github.io/api/#entry-points
-            outfile: './clients/vscode/dist/server.js',
-            platform: 'browser',
-            format: 'cjs',
-        }),
+        })
     ]);
 } catch (e) {
     console.error(e);
@@ -52,13 +39,10 @@ try {
 if (watch) {
     //await desktopContext.watch();
     await browserContext.watch();
-    await serverContext.watch();
 } else {
     //desktopContext.rebuild();
     browserContext.rebuild();
-    serverContext.rebuild();
 
     //desktopContext.dispose();
     browserContext.dispose();
-    serverContext.dispose();
 }
